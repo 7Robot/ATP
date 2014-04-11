90b208bc9bd82809f2bef2f86d3709d2097d155d
Élie Bouttier <elie@bouttier.eu.org>
2014-04-11 04:00:07 +0200

    Add atp python module

---
+++ atp/atp/__init__.py
@@ -0,0 +1,1 @@
+from atp import encode, decode
---
+++ atp/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ATP',
+    version='0.1.0',
+    author='Élie Bouttier',
+    author_email='elie+7robot@bouttier.eu.org',
+    packages=['atp'],
+    scripts=['bin/atp-encode', 'bin/atp-decode'],
+    url='http://7robot.fr',
+    description='7Robot Eurobot - ATP module.',
+)
